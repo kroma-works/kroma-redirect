8e33032532d219f80410455e59f4c6dda69bfb57
Jason Aviandre <jasonaviandre@gmail.com>
2024-04-05 13:55:24 +0800

    ⚙️ refactor: move link to js

---
+++ index.js
@@ -0,0 +1,13 @@
+var linkTag = document.createElement("link");
+linkTag.setAttribute("rel", "canonical");
+linkTag.setAttribute("href", "https://kroma.works");
+
+// TODO: make it dynamic, maybe even fetch from DB
+const url = "https://kroma.works";
+
+var metaTag = document.createElement("meta");
+metaTag.setAttribute("http-equiv", "refresh");
+metaTag.setAttribute("content", `0; url=${url}`);
+
+document.getElementsByTagName("head")[0].appendChild(linkTag);
+document.getElementsByTagName("head")[0].appendChild(metaTag);
